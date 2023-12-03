55e77be1d83829ed43a4aaf91b9afd9b5f65fbbd
CeanD18 <139362413+CeanD18@users.noreply.github.com>
2023-12-03 21:03:44 +0700

    Change index.html file

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+alert('Chào cậu, chúc một ngày tốt lành')
+var tuoi = prompt('Cấm trẻ dưới 18. Vui lòng nhập tuổi')
+    if (tuoi < 18)
+{
+    setTimeout(function() {
+    alert('Đã đủ tuổi đâu :))?')
+}, 3000)
+}
+
+    else
+{
+    setTimeout(function() {
+    alert('Đã đủ tuổi đi tù')},3000)
+}
 const $list = document.querySelectorAll('li');
 
 function activeLink() {
