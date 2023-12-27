1a33e06418e0a4090ff616fc1d1f5e6846676eab
CeanD18 <139362413+CeanD18@users.noreply.github.com>
2023-12-27 22:16:39 +0700

    Your commit message here

--- scrip.js
+++ scrip.js
@@ -1,10 +1,1 @@
-function showScreen(screenId) {
-    var screens = document.querySelectorAll('.screen');
-    screens.forEach(function(screen) {
-        if (screen.id === screenId) {
-            screen.classList.add('active');
-        } else {
-            screen.classList.remove('active');
-        }
-    });
-}
+
